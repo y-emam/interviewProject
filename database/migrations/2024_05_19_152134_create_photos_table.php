593fef946bd0a05f56160931694ae956ae331db9
y-emam <YasserEmam2002@gmail.com>
2024-05-19 19:02:38 +0300

    created migrations and routes

---
+++ database/migrations/2024_05_19_152134_create_photos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->uuid('id')->default(DB::raw('(UUID())'));
+            $table->string('name');
+            $table->string('path');
+            $table->uuid('album_id');
+            $table->timestamps();
+
+            $table->foreign('album_id')->references('id')->on('albums')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('photos', function (Blueprint $table) {
+            $table->dropForeign(['album_id']);
+        });
+
+        Schema::dropIfExists('photos');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/home', function () {});
+// Route::get('/create', );
+Route::get('/edit');
+Route::get('/delete');
+
+// Route::post('/create', );
+
+// Route::put('/edit', );
+
+// Route::delete('/deleteAllAlbum', );
+// Route::delete('/deleteOnlyAlbum', );
